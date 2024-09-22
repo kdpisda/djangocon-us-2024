79f57ea8ffc267e01d0848e1dc2194eee0d58aa6
Kuldeep Pisda <pisdak79@gmail.com>
2024-09-22 01:54:12 -0400

    Fixed custom user model

--- iam/models.py
+++ iam/models.py
@@ -1,3 +1,20 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from utils.mixins.models.timestamps import TimeStampedModel
+
+
+class GenderChoice(models.TextChoices):
+    MALE = "MALE", "Male"
+    FEMALE = "FEMALE", "Female"
+    OTHER = "OTHER", "Other"
+    NO_DISCLOSE = "NO_DISCLOSE", "Prefer not to disclose"
+
+
+class User(TimeStampedModel, AbstractUser, models.Model):
+    gender = models.CharField(
+        choices=GenderChoice.choices, max_length=15, default=GenderChoice.NO_DISCLOSE
+    )
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}".strip() or self.username
--- tutorial/settings.py
+++ tutorial/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "iam",
+    "cms",
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,5 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+AUTH_USER_MODEL = "iam.User"
