a8eeb46cbf7602cbc9c0025abed5a3703def8066
Kuldeep Pisda <pisdak79@gmail.com>
2024-09-22 04:20:30 -0400

    FIxed user admin

--- iam/admin.py
+++ iam/admin.py
@@ -1,3 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+
+User = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = (
+        "id",
+        "first_name",
+        "last_name",
+        "email",
+        "gender",
+        "is_active",
+        "is_staff",
+        "is_superuser",
+    )
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (
+            _("Personal info"),
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "gender",
+                )
+            },
+        ),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
